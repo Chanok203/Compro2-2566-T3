4c681fa137a7e26e1771def64a3dcc59db2c7677
unknown <cpathompatai@gmail.com>
2024-04-18 14:24:20 +0700

    add Partition1

---
+++ src/Partition1.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Partition1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = sc.nextInt();
+        }
+        int ci = arr.length / 2;
+        int cv = arr[ci];
+        // System.out.println("arr: " + Arrays.toString(arr));
+        // System.out.println("center_index: " + ci);
+        // System.out.println("center_value: " + cv);
+
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] <= cv) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] > cv) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+
+    }
+}
